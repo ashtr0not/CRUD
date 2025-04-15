d8ecebf7f8e4717b8c9ceab60f0378ebe352c0ae
Ash Carey DEV <ashtr0not21@gmail.com>
2025-04-15 10:35:19 -0700

    Updated Like Button

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 const express = require('express') // we are requiring express and we will consume it with code in line 2
 const app = express() //wherever you see app, this is really just express 
 const MongoClient = require('mongodb').MongoClient //this package helps us connect to our database 
-const cors = require('cors')
+
 const PORT = 2121 // this is setting our port 
 
-app.use(cors())
+require('dotenv').config() // This has also been added alongside line 16. This means use dotenv and run it
+//There should now be a separate .env file within your folder to store the server link
+
+
 //Setting up Required Modules 
 
 //Database Set Up
 let db, // this is a global variable
-    dbConnectionStr = 'mongodb+srv://ashcarey0421:Neli-Ash-and-Kaleo@testcluster.ydsl9q8.mongodb.net/', // this connection string is given by Mongo Atlas that is given when creating a cluster
+    //dbConnectionStr = 'mongodb+srv://ashcarey0421:Neli-Ash-and-Kaleo@testcluster.ydsl9q8.mongodb.net/', // this connection string is given by Mongo Atlas that is given when creating a cluster
+    dbConnectionStr = process.env.DB_STRING, //These are environment variables to hold our sensitive data
     dbName = 'rap' // this is the name of the actual database 
 
 MongoClient.connect(dbConnectionStr) // this connects our application to our db. 
@@ -31,7 +35,7 @@ app.use(express.json()) // this line and the one above enable us to look at the 
 
 // GET Request 
 app.get('/', (request, response) => {
-    db.collection('rappers').find().toArray()
+    db.collection('rappers').find().sort({likes: -1}).toArray() // adding the .sort({likes: -1}) sorts the list by highest likes 
     .then(data => { // data is the array that holds the objects 
         response.render('index.ejs', {info: data}) //we are assigning the data to the cariable info which is used in our EJS file
         //the render is what spits out html from that ejs file 
@@ -42,7 +46,9 @@ app.get('/', (request, response) => {
 
 //POST Request
 app.post('/addRapper', (request,response) => {
-    db.collection('rappers').insertOne(request.body) //the items inputted into the form is not the request.body
+    db.collection('rappers').insertOne({stageName: request.body.stageName,
+        birthName: request.body.birthName, likes: 0}) //the items inputted into the form is not the request.body
+    // db.collection('rappers).insertOne({zebra: request.body.a, unicorn: request.body.b}) => in out data base we will see zebra: a and unicorn: b
     .then(result =>{
         console.log('Rapper Added')
         response.redirect('/')
@@ -60,6 +66,40 @@ app.delete('/deleteRapper', (request,response) => {
     })
     .catch(error => console.error(error))
 })
+
+app.put('/likeRapper', (request, response) => {
+    db.collection('rappers').updateOne({stageName: request.body.stageNameS,
+        birthName: request.body.birthNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+        }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+// app.put('/likeRapper', (request,response) => {
+//     db.collection('rappers').updateOne({stageName: request.body.stageNameS,
+//         birthName: request.body.birthNameS, likes: request.body.likesS},{
+//             $set: {
+//                 likes: request.body.likesS + 1 
+//             }
+//         },{
+//             sort: {_id: -1}, // This line says to look in descending order to find the match because there could be multiple properties with the same names 
+//             upsert: true // this means if you dont find the match, create a new document that has all this informaion
+//         })
+//         .then(result => {
+//             console.log('Added One Like')
+//             response.json('Like Added')
+//         })
+// })
 //This whole section makes up our API
 
 //Setting up the app to listen on the port set on line 5
